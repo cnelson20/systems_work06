5e184f7bec67f0f9307df90338baa9d77eb1c582
cnelson20 <cjnelson2004@gmail.com>
2021-10-01 11:38:53 -0400

    wrote the code

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+  int array[10];
+  array[0] = 0;
+  int i;
+  srand(time(NULL));
+  for (i = 1; i < (sizeof(array) / sizeof(int)); i++) {
+    array[i] = rand();
+  }
+  printf("{");
+  for (i = 0; i < (sizeof array / sizeof(int)); i++) {
+    if (i != 0) {printf(", ");}
+    printf("%d",array[i]);
+  }
+  printf("}\n");
+
+  int revarray[10];
+  int *revpointer = revarray;
+  i = 0;
+  int j;
+  for (j = (sizeof array / sizeof(int)) - 1; j >= 0; j--) {
+    revpointer[j] = array[i];
+    i++;
+  }
+  printf("{");
+  for (i = 0; i < (sizeof revarray / sizeof(int)); i++) {
+    if (i != 0) {printf(", ");}
+    printf("%d",*(revpointer+i));
+  }
+  printf("}\n");
+}
